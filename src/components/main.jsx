121f085d0e4ddd72bb6aeac730513aa268022839
Maneet08 <abhimanyutokas@gmail.com>
2018-10-28 23:07:08 +0530

    Router Implemented

---
+++ src/components/main.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import LandingPage from "./landingpage";
+import About from "./aboutme";
+import Contact from "./contact";
+import projects from "./projects";
+import Resume from "./resume";
+import { Switch, Route } from "react-router-dom";
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+    <Route path="/aboutme" component={About} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={projects} />
+    <Route path="/resume" component={Resume} />
+  </Switch>
+);
+
+export default Main;
