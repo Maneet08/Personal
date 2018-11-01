6fa85b30e24546b8df8cbefdce25075a80e26595
Maneet08 <abhimanyutokas@gmail.com>
2018-11-01 21:44:04 +0530

    Resume

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,10 +1,79 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Education from "./education";
+import Experience from "./experience";
+import Skills from "./skills";
 
 class Resume extends Component {
   render() {
     return (
       <div>
-        <h1>Resume Page</h1>
+        <Grid>
+          <Cell col={4}>
+            <div style={{ textAlign: "center" }}>
+              <img
+                src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
+                style={{ height: "200px" }}
+              />
+            </div>
+
+            <h2 style={{ paddingTop: "1em" }}>Maneet Chaudhary</h2>
+            <h3 style={{ color: "grey" }}>Programmer</h3>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <h5>Address</h5>
+            <p>H.No. 1 Mohammadpur, New Delhi</p>
+            <h5>Phone</h5>
+            <p>(123) 456-7890</p>
+            <h5>Email</h5>
+            <p>someone@example.com</p>
+            <h5>Web</h5>
+            <p>mywebsite.com</p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+            <Education
+              startYear="2015"
+              endYear="2018"
+              schoolName="XYZ"
+              schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            />
+
+            <Education
+              startYear="2011"
+              endYear="2015"
+              schoolName="ABC"
+              schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            />
+            <hr style={{ borderTop: "3px solid #e22947" }} />
+
+            <h2>Experience</h2>
+            <Experience
+              startYear="2009"
+              endYear="2012"
+              jobName="MyFirstJob"
+              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            />
+            <Experience
+              startYear="2012"
+              endYear="2016"
+              jobName="MySecondJob"
+              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            />
+            <hr style={{ borderTop: "3px solid #e22947" }} />
+            <h2>Skills</h2>
+            <Skills skill="Javascript" progress="100" />
+            <Skills skill="HTML/CSS" progress="100" />
+            <Skills skill="JS" progress="100" />
+          </Cell>
+        </Grid>
       </div>
     );
   }
